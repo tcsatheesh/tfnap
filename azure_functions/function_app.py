4db63b460f635b96c32f7c1cb1301e330d2bdea7
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-24 18:48:01 +0000

    try again

--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -1,44 +1,27 @@
-"""Azure Function to run periodically."""
-
-import os
+import azure.functions as func
+import datetime
 import json
 import logging
-from datetime import datetime, timezone
 
-import azure.functions as func
+app = func.FunctionApp()
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
-logging.getLogger("azure").setLevel(logging.WARNING)
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
 
-_logger = logging.getLogger(__name__)
-
-# Heartbeat service to check Azure Function health and build number
-@app.route(route="heartbeat", methods=["GET"])
-def heartbeat(req: func.HttpRequest) -> func.HttpResponse:
-    """Heartbeat API to check the health of the Azure function
-
-    Args:
-        req (func.HttpRequest): requesting status
-
-    Returns:
-        func.HttpResponse: response with live status
-    """
-
-    _build_id = os.getenv("BUILD_ID", "Local Build")
-    _logger.info(
-        "Build id %s: Heartbeat request received @%s. Time is in UTC.",
-        _build_id,
-        datetime.utcnow(),
-    )
-    _json_data = {
-        "build_id": _build_id,
-        "datetime": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-        "status": "Function App is running",
-    }
-    _json_data = json.dumps(_json_data)
-    return func.HttpResponse(
-        _json_data,
-        headers={"Content-Type": "application/json"},
-        status_code=200,
-    )
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
