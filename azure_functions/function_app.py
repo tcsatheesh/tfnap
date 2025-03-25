4b4132bb74acdba3f0c8268555951d602e5ba2ed
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-25 10:18:37 +0000

    add build id

--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -1,4 +1,5 @@
 import azure.functions as func
+import os
 import datetime
 import json
 import logging
@@ -7,7 +8,10 @@ app = func.FunctionApp()
 
 @app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+
+    _build_id = os.getenv("BUILD_ID", "Local Build")
+
+    logging.info('Python HTTP trigger function processed a request for build id %s.', _build_id)
 
     name = req.params.get('name')
     if not name:
@@ -19,9 +23,9 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully for build id {_build_id}.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response for build id {_build_id}.",
              status_code=200
         )
